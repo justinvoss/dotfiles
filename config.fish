8555ffe1455f126a95c46069a9121faf89eb1a7f
Justin Voss <justin@bleedingwolf.com>
2012-06-08 18:14:34 -0700

    added config for fishfish shell

---
+++ config.fish
@@ -0,0 +1,25 @@
+
+# loves me some vim
+set -x EDITOR vim
+
+# handy shortcuts
+function gs
+  git status
+end
+
+function ss
+  svn status
+end
+
+# that stylish prompt
+function fish_prompt
+  set_color green
+  printf (hostname|cut -d . -f 1)
+  set_color normal
+  printf ':'
+  set_color white
+  printf (prompt_pwd)
+  set_color normal
+  printf '\n$ '
+end
+
