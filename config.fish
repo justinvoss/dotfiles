321a35a02ea3ca6a672228bf25dcc2199e3b7d3b
Justin Voss <justin@bleedingwolf.com>
2012-07-19 11:02:49 -0700

    add Git branch info the command prompt

--- config.fish
+++ config.fish
@@ -11,6 +11,15 @@ function ss
   svn status
 end
 
+function parse_git_branch
+  set ref (git symbolic-ref HEAD 2> /dev/null)
+  if [ $ref ]
+    printf " ("
+    printf (echo $ref | sed 's/refs\/heads\///g')
+    printf ")"
+  end
+end
+
 # that stylish prompt
 function fish_prompt
   set_color green
@@ -19,6 +28,8 @@ function fish_prompt
   printf ':'
   set_color white
   printf (prompt_pwd)
+  set_color blue
+  parse_git_branch
   set_color normal
   printf ' $ '
 end
